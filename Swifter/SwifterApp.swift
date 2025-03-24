af7e670e8140fcad617562c35b5b6dc3eec32819
Adeline Charlotte Augustinne <adelinec@10-60-58-206.0200.bsd.id.iosda.org>
2025-03-24 12:08:31 +0700

    Initial commit

---
+++ Swifter/SwifterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifterApp.swift
+//  Swifter
+//
+//  Created by Adeline Charlotte Augustinne on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwifterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

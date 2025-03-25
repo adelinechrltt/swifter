7eccc50bac3ca21141f46a45b901ce5e03d7465a
Adeline Charlotte Augustinne <adelinec@10-60-58-206.0200.bsd.id.iosda.org>
2025-03-25 11:13:46 +0700

    Testing out navbar & tab bar

---
+++ Swifter/TabView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  Swifter
+//
+//  Created by Adeline Charlotte Augustinne on 24/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
+
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gearshape")
+                }
+        }
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        NavigationStack {
+            Text("🏠 Home Screen")
+                .font(.largeTitle)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        HStack {
+                            Text("Hot Topics")
+                                .font(.largeTitle)
+                            Spacer()
+                            Image(systemName: "star.fill")
+                                .foregroundStyle(.orange)
+                        }.padding(20)
+                    }
+                }
+        }
+    }
+}
+    
+    struct ProfileView: View {
+        var body: some View {
+            Text("👤 Profile Screen")
+                .font(.largeTitle)
+        }
+    }
+    
+    struct SettingsView: View {
+        var body: some View {
+            Text("⚙️ Settings Screen")
+                .font(.largeTitle)
+        }
+    }
+    
+    #Preview {
+        ContentView()
+    }

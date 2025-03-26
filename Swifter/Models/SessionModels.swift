55c5aa25b1f9d0cb6cd9568dbb81ed65899f19e2
Adeline Charlotte Augustinne <adelinec@10-60-58-206.0200.bsd.id.iosda.org>
2025-03-26 15:54:13 +0700

    Updated joggingSession class

---
+++ Swifter/Models/SessionModels.swift
@@ -0,0 +1,42 @@
+//
+//  JogSession.swift
+//  Swifter
+//
+//  Created by Adeline Charlotte Augustinne on 26/03/25.
+//
+
+import Foundation
+import EventKit
+
+enum SessionType {
+    case prejog
+    case jogging
+    case postjog
+}
+
+enum isCompleted : String {
+    case completed = "Done"
+    case incomplete = "Not started yet"
+    case missed = "Missed"
+}
+
+class SessionModel: Identifiable {
+    let id = UUID()
+        
+    var startTime: Date
+    var endTime: Date
+    var distance: Int?
+    var calendarEvent: EKEvent
+    
+    // enums
+    var sessionType: SessionType
+    var status: isCompleted
+    
+    init(startTime: Date, endTime: Date, calendarEvent: EKEvent, sessionType: SessionType){
+        self.startTime = startTime
+        self.endTime = endTime
+        self.calendarEvent = calendarEvent
+        self.sessionType = sessionType
+        self.status = isCompleted.incomplete
+    }
+}

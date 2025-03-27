03f1aabec0997469fd9e51dc1854dfe3dd90b153
Teuku Fazariz Basya <teukufazarizbasya@gmail.com>
2025-03-27 12:59:13 +0700

    Model

---
+++ Swifter/Models/PreferencesModel.swift
@@ -0,0 +1,59 @@
+//
+//  PreferencesModel.swift
+//  Swifter
+//
+//  Created by Teuku Fazariz Basya on 26/03/25.
+//
+
+import Foundation
+import SwiftData
+
+enum TimeOfDay: String, CaseIterable, Identifiable, Codable{
+    case morning = "Morning"
+    case afternoon = "Afternoon"
+    case evening = "Evening"
+    case night = "Night"
+
+    var id: String { self.rawValue }
+}
+
+enum DayOfWeek: Int, CaseIterable, Identifiable, Codable{
+    case monday = 2
+    case tuesday = 3
+    case wednesday = 4
+    case thursday = 5
+    case friday = 6
+    case saturday = 7
+    case sunday = 1
+
+    var id: Int { self.rawValue }
+
+    var name: String {
+        switch self {
+        case .monday : return "Monday"
+        case .tuesday : return "Tuesday"
+        case .wednesday : return "Wednesday"
+        case .thursday : return "Thursday"
+        case .friday : return "Friday"
+        case .saturday : return "Saturday"
+        case .sunday : return "Sunday"
+        }
+    }
+}
+
+class PreferencesModel: Identifiable {
+    let id = UUID()
+
+    var preJogDuration: Int
+    var postJogDuration: Int
+
+    var preferredTimesOfDay: Set<TimeOfDay>
+    var preferredDaysOfWeek: Set<DayOfWeek>
+
+    init(timeOfDay: Set<TimeOfDay>, dayOfWeek: Set<DayOfWeek>, preJogDuration: Int, postJogDuration: Int) {
+        self.preferredTimesOfDay = timeOfDay
+        self.preferredDaysOfWeek = dayOfWeek
+        self.preJogDuration = preJogDuration
+        self.postJogDuration = postJogDuration
+    }
+}

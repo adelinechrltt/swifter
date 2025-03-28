9b927531eb3f3586ba2aab6505001393e103c04a
Teuku Fazariz Basya <teukufazarizbasya@gmail.com>
2025-03-28 20:16:00 +0700

    merge

---
+++ Swifter/Views/EditSessionView.swift
@@ -0,0 +1,56 @@
+//
+//  EditSession.swift
+//  Swifter
+//
+//  Created by Adeline Charlotte Augustinne on 27/03/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct EditSession: View {
+    
+    @EnvironmentObject private var eventStoreManager: EventStoreManager
+    @StateObject private var viewModel: EditSessionViewModel
+    
+    @State private var showAlert = false
+    @State private var showEventEditor = false
+    @State private var errorText = ""
+
+    // init to inject environment object
+    init() {
+        let manager = EventStoreManager()
+        _viewModel = StateObject(wrappedValue: EditSessionViewModel(eventStoreManager: manager))
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                HStack{
+                    Text("Edit Session")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                    Button {
+                        viewModel.createNewEvent(
+                                eventTitle: "Jogging",
+                                startTime: viewModel.startJog,
+                                endTime: viewModel.endJog
+                        )
+                        showEventEditor = false
+                    } label: {
+                        Text("Save")
+                    }
+                }
+                
+                DurationPicker(pickerLabel: "Pre-jog", startTime: $viewModel.startPrejog, endTime: $viewModel.endPrejog)
+                DurationPicker(pickerLabel: "Jog", startTime: $viewModel.startJog, endTime: $viewModel.endJog)
+                DurationPicker(pickerLabel: "Post-jog", startTime: $viewModel.startPostjog, endTime: $viewModel.endPostjog)
+                }
+            }.padding()
+        }
+}
+
+#Preview {
+    EditSession().environmentObject(EventStoreManager())
+}
